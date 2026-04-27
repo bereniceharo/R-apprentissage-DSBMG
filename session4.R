e2624fe91fd25a1eb40dd1a6bd8d39ce5639aed5
bereniceharo <pielaen@hotmail.com>
2026-04-27 21:09:30 +0000

    ifelse

---
+++ session4.R
@@ -0,0 +1,31 @@
+taux <- c(0.91, 0.45, 0.83, 0.38, 0.76)
+
+statut <- ifelse(taux >= 0.8, "objectif atteint", "alerte")
+statut
+library(readxl)
+indic2025 <- read_excel("Indicateurs_FS_2025.xlsx", sheet = "FS_2025")
+str(indic2025)
+indic2025$`CibleFS-Couverture en CPN4 2025 FS Public`
+seuil_cpn4 <- 80
+indic2025$statut_cpn4 <- ifelse(indic2025$`CibleFS-Couverture en CPN4 2025 FS Public` >= 80,
+                               "objectif atteint", "alerte")
+indic2025$statut_cpn4
+View(indic2025)
+view(indic2025)
+head(indic2025)
+str(indic2025)
+indic2025 <- indic2025[-indic2025$satut_cpn4]
+names(indic2025)
+indic2025$satut_cpn4 <- NULL
+names(indic2025)
+factor(indic2025$statut_cpn4, labels = "objectif atteint", "alerte")
+factor()
+factor
+table(indic2025$statut_cpn4)
+
+table(indic2025$statut_cpn4)
+
+
+
+
+
